7d25b15f8ae8ee81ec763e3e93c19448258c49c5
Sofi <sofiwesson@gmail.com>
2025-02-18 22:36:04 +1100

    added chip reserve, changed chip spawning, started selection detection

--- othello/Scripts/boardLayout.gd
+++ othello/Scripts/boardLayout.gd
@@ -2,8 +2,14 @@ extends Node
 
 var gizmo = preload("res://Prefabs/Gizmo.tscn")
 var chipPrefab = preload("res://Prefabs/Chip.tscn")
+var selectionCollider = preload("res://Prefabs/SelectionCollider.tscn")
+
+var p1ChipReserve: ChipReserve
+var p2ChipReserve: ChipReserve
 
 var matrixSlots: Array[Array] = []
+
+var isPlayerOnesTurn: bool
 
 enum SlotState {
 	EMPTY,
@@ -18,13 +24,21 @@ class Slot:
 
 	var slotState: SlotState
 	var position: Vector3
+	var selectionCollider: Node3D
 	var chip: Node3D
+
+class ChipReserve:
+	var chipReserve: Node3D
+	var chips: Array
+	var chipSpacing = 0.25
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	isPlayerOnesTurn = true
+
 	MakeMatrix()
 	MakeGrid()
-	SpawnChips()
+	MakeChipReserves()
 	
 func MakeMatrix():
 	#add columns to matrix (y)
@@ -38,30 +52,42 @@ func MakeMatrix():
 func MakeGrid():
 	#populate the grid slots
 	var y = -3.5
-	for row in matrixSlots: # y (actually z for some)
+	for row in matrixSlots: # y (actually z for some reason)
 		var x = -3.5
 		for space in row: # x
 			space.slotState = SlotState.EMPTY
 			space.position = Vector3(x, 0, y)
+			space.selectionCollider = selectionCollider.instantiate()
+			space.selectionCollider.position = space.position
 			x += 1
 		y += 1
 
-func SpawnChips():
-	#spawn chips in place
-	for row in matrixSlots:
-		for space in row:
-			var newChip = chipPrefab.instantiate()
-			newChip.position = space.position
-			newChip.visible = false
-			space.chip = newChip
-			add_child(space.chip)
+func MakeChipReserves():
+	#set up the chip reserves for player 1
+	p1ChipReserve = ChipReserve.new()
+	p1ChipReserve.chipReserve = get_node("P1ChipReserve")
+	SpawnChips(p1ChipReserve)
+	#set up the chip reserves for player 2
+	p2ChipReserve = ChipReserve.new()
+	p2ChipReserve.chipReserve = get_node("P2ChipReserve")
+	SpawnChips(p2ChipReserve)
+
+func SpawnChips(chipReserve: ChipReserve):
+	#spawn 32 chips in each reserve
+	for i in range(32):
+		var newChip = chipPrefab.instantiate()
+		chipReserve.chipReserve.add_child(newChip)
+		newChip.position = Vector3(0.15 + (i * chipReserve.chipSpacing), 0, 0)
+		var rad = deg_to_rad(90)
+		newChip.rotate_z(rad)
+		chipReserve.chips.push_back(newChip)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
 
 func PlaceChip():
 	pass
 
 func FlipChip():
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
 	pass
